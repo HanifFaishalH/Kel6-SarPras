708acc88b56bc593338b0c74e5dd7ff3b1200b7b
Aryo Adi Putro <aryoadiputro@gmail.com>
2025-05-09 21:27:04 +0700

    template initiation

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,118 @@
+<!-- sidebar menu area start -->
+<div class="sidebar-menu">
+    <div class="sidebar-header">
+        <div class="logo">
+            <a href="{{ asset('srtdash/index.html') }}"><img src="{{ asset('srtdash/assets/images/icon/logo.png') }}" alt="logo"></a>
+        </div>
+    </div>
+    <div class="main-menu">
+        <div class="menu-inner">
+            <nav>
+                <ul class="metismenu" id="menu">
+                    <li class="active">
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                        <ul class="collapse">
+                            <li class="active"><a href="{{ asset('srtdash/index.html') }}">ICO dashboard</a></li>
+                            <li><a href="{{ asset('srtdash/index2.html') }}">Ecommerce dashboard</a></li>
+                            <li><a href="{{ asset('srtdash/index3.html') }}">SEO dashboard</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Sidebar Types</span></a>
+                        <ul class="collapse">
+                            <li><a href="{{ asset('srtdash/index.html') }}">Left Sidebar</a></li>
+                            <li><a href="{{ asset('srtdash/index3-horizontalmenu.html') }}">Horizontal Sidebar</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-pie-chart"></i><span>Charts</span></a>
+                        <ul class="collapse">
+                            <li><a href="{{ asset('srtdash/barchart.html') }}">bar chart</a></li>
+                            <li><a href="{{ asset('srtdash/linechart.html') }}">line Chart</a></li>
+                            <li><a href="{{ asset('srtdash/piechart.html') }}">pie chart</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-palette"></i><span>UI Features</span></a>
+                        <ul class="collapse">
+                            <li><a href="{{ asset('srtdash/accordion.html') }}">Accordion</a></li>
+                            <li><a href="{{ asset('srtdash/alert.html') }}">Alert</a></li>
+                            <li><a href="{{ asset('srtdash/badge.html') }}">Badge</a></li>
+                            <li><a href="{{ asset('srtdash/button.html') }}">Button</a></li>
+                            <li><a href="{{ asset('srtdash/button-group.html') }}">Button Group</a></li>
+                            <li><a href="{{ asset('srtdash/cards.html') }}">Cards</a></li>
+                            <li><a href="{{ asset('srtdash/dropdown.html') }}">Dropdown</a></li>
+                            <li><a href="{{ asset('srtdash/list-group.html') }}">List Group</a></li>
+                            <li><a href="{{ asset('srtdash/media-object.html') }}">Media Object</a></li>
+                            <li><a href="{{ asset('srtdash/modal.html') }}">Modal</a></li>
+                            <li><a href="{{ asset('srtdash/pagination.html') }}">Pagination</a></li>
+                            <li><a href="{{ asset('srtdash/popovers.html') }}">Popover</a></li>
+                            <li><a href="{{ asset('srtdash/progressbar.html') }}">Progressbar</a></li>
+                            <li><a href="{{ asset('srtdash/tab.html') }}">Tab</a></li>
+                            <li><a href="{{ asset('srtdash/typography.html') }}">Typography</a></li>
+                            <li><a href="{{ asset('srtdash/form.html') }}">Form</a></li>
+                            <li><a href="{{ asset('srtdash/grid.html') }}">grid system</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-slice"></i><span>icons</span></a>
+                        <ul class="collapse">
+                            <li><a href="{{ asset('srtdash/fontawesome.html') }}">fontawesome icons</a></li>
+                            <li><a href="{{ asset('srtdash/themify.html') }}">themify icons</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-table"></i><span>Tables</span></a>
+                        <ul class="collapse">
+                            <li><a href="{{ asset('srtdash/table-basic.html') }}">basic table</a></li>
+                            <li><a href="{{ asset('srtdash/table-layout.html') }}">table layout</a></li>
+                            <li><a href="{{ asset('srtdash/datatable.html') }}">datatable</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="{{ asset('srtdash/maps.html') }}"><i class="ti-map-alt"></i> <span>maps</span></a></li>
+                    <li><a href="{{ asset('srtdash/invoice.html') }}"><i class="ti-receipt"></i> <span>Invoice Summary</span></a></li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layers-alt"></i><span>Pages</span></a>
+                        <ul class="collapse">
+                            <li><a href="{{ asset('srtdash/login.html') }}">Login</a></li>
+                            <li><a href="{{ asset('srtdash/login2.html') }}">Login 2</a></li>
+                            <li><a href="{{ asset('srtdash/login3.html') }}">Login 3</a></li>
+                            <li><a href="{{ asset('srtdash/register.html') }}">Register</a></li>
+                            <li><a href="{{ asset('srtdash/register2.html') }}">Register 2</a></li>
+                            <li><a href="{{ asset('srtdash/register3.html') }}">Register 3</a></li>
+                            <li><a href="{{ asset('srtdash/register4.html') }}">Register 4</a></li>
+                            <li><a href="{{ asset('srtdash/screenlock.html') }}">Lock Screen</a></li>
+                            <li><a href="{{ asset('srtdash/screenlock2.html') }}">Lock Screen 2</a></li>
+                            <li><a href="{{ asset('srtdash/reset-pass.html') }}">reset password</a></li>
+                            <li><a href="{{ asset('srtdash/pricing.html') }}">Pricing</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-exclamation-triangle"></i><span>Error</span></a>
+                        <ul class="collapse">
+                            <li><a href="{{ asset('srtdash/404.html') }}">Error 404</a></li>
+                            <li><a href="{{ asset('srtdash/403.html') }}">Error 403</a></li>
+                            <li><a href="{{ asset('srtdash/500.html') }}">Error 500</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-align-left"></i> <span>Multi level menu</span></a>
+                        <ul class="collapse">
+                            <li><a href="{{ asset('srtdash/item1.html') }}">Item level (1)</a></li>
+                            <li><a href="{{ asset('srtdash/item2.html') }}">Item level (1)</a></li>
+                            <li><a href="javascript:void(0)" aria-expanded="true">Item level (1)</a>
+                                <ul class="collapse">
+                                    <li><a href="{{ asset('srtdash/item3.html') }}">Item level (2)</a></li>
+                                    <li><a href="{{ asset('srtdash/item4.html') }}">Item level (2)</a></li>
+                                    <li><a href="{{ asset('srtdash/item5.html') }}">Item level (2)</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="{{ asset('srtdash/item6.html') }}">Item level (1)</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+<!-- sidebar menu area end -->
